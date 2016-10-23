6d94df1be62601ba5553b24e6875edc936c7bda1
cloud <861525092@qq.com>
2016-10-23 19:21:24 +0800

    test code

---
+++ 6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+x = raw_input ('please input you name:')
+y = raw_input ('please input you age:')
+if x == 'cloud':
+    if y == '20':
+        print 'you are yang boy'
+    elif y == '10':
+        print 'you are somale girl'
+    else:
+        print 'unno is age'
+else:
+    print 'error'
