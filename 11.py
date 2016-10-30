02a2de1872711c689e6bc7ab1600e0c11e1d68dc
cloud <861525092@qq.com>
2016-10-30 23:57:30 +0800

    while test

---
+++ 11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#conding=UTF-8
+sum = 0
+num = ''
+while num != 'pc':
+    num = raw_input('please input num:')
+    if num != 'pc':
+            sum = sum + int(num)
+print sum
+
+
